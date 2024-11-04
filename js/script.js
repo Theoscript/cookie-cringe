b4feeed3efecb7897acbfbfa81a833b244983139
Theo Snyman <heytheosnyman@gmail.com>
2024-11-04 22:28:57 +0200

    Fixed broken SVG file path for real
    See code comment for why it was broken.

--- js/script.js
+++ js/script.js
@@ -72,8 +72,13 @@ modalForm.addEventListener("submit", function (submit) {
   const userNameSurname = modalFormData.get("userNameSurname");
 
   document.querySelector(".modal__inner-form").style.display = "none";
+  /**
+   * for the file path below think in terms of your index.html file
+   * because we are inserting this content into that doc, so the relative
+   * file path would need to be relative from that document
+   */
   modalText.innerHTML = `<div class="modal__inner-loading">
-                          <img src="/assets/images/loading.svg" class="loading" />
+                          <img src="./assets/images/loading.svg" class="loading" />
                           <p id="modal__inner-upload-text">
                             Uploading <span class="modal__display-name">${userNameSurname}'s</span> information to the dark web 😈
                           </p>
