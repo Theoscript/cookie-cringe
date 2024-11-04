6b89f862b219269dfc2137fbf46027325494a9a2
Theo Snyman <heytheosnyman@gmail.com>
2024-11-04 19:30:16 +0200

    Implemented modal form submission
    Fixed modal overflow in the process.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 const modal = document.querySelector("#modal");
 const modalBtnClose = document.querySelector("#modal__btn-close");
+const modalForm = document.querySelector(".modal__inner-form");
+const modalFormInputNameSurname = document.querySelector(
+  "#form__user-name-surname"
+);
+const modalFormInputEmail = document.querySelector("#form__user-email");
+const modalText = document.querySelector("#modal__inner-text");
 
 setTimeout(function () {
   /**
@@ -12,4 +18,16 @@ setTimeout(function () {
 
 modalBtnClose.addEventListener("click", () => {
   modal.style.display = "none";
+});
+
+modalForm.addEventListener("submit", function (submit) {
+  submit.preventDefault();
+  console.log("Form Submitted!");
+  modalFormInputNameSurname.value = "";
+  modalFormInputEmail.value = "";
+  modalText.innerHTML = `<div class="modal__inner-loading">
+                          <img src="../assets/images/loading.svg" class="loading" />
+                          <p id="modal__inner-upload-text">
+                            Uploading your information to the dark web 😈
+                          </p>`;
 });
