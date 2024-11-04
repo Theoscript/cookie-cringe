91c3517832013427828cb61213ff45688cda6be3
Theo Snyman <heytheosnyman@gmail.com>
2024-11-04 15:21:20 +0200

    Hooked up the modal close button

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
 const modal = document.querySelector("#modal");
+const modalBtnClose = document.querySelector("#modal__btn-close");
 
 setTimeout(function () {
+  /**
+   * setting the modal to display block is more
+   * consistent in layout and cross browser functionality
+   * compared to setting it to inline
+   */
   modal.style.display = "block";
 }, 3500);
+
+modalBtnClose.addEventListener("click", () => {
+  modal.style.display = "none";
+});
